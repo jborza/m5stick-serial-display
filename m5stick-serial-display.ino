48be5ac1e937c6b4c31b567c385f606709947c06
jborza <jborza@gmail.com>
2020-11-12 22:07:25 +0100

    using rtos Task API

--- m5stick-serial-display.ino
+++ m5stick-serial-display.ino
@@ -3,12 +3,11 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/timers.h>
 #include <freertos/task.h>
-#include <freertos/queue.h>
 
 #define DISPLAY_LINE_LENGTH 240
 #define RECEIVE_LINE_BITS 120
 #define RECEIVE_LINE_BYTES 25//120/8
-#define BAUD_RATE 9600
+#define BAUD_RATE 500000
 #define HSYNC 0x48
 #define VSYNC 0x56
 
@@ -23,85 +22,86 @@ int row = 0;
 
 const int ledPin = 10;
 
+//task that handles the display part
 TaskHandle_t displayTask;
-QueueHandle_t vidQueue;
-QueueHandle_t doneQueue;
 
-#define QUEUE_DELAY ( TickType_t ) 100
+//task that handles the receiver part
+TaskHandle_t serialTask;
+
 
 void display_task( void * parameter) {
-  int *queueData = NULL;
-  //initial notify to UART task (loop) that we can receive data
-  xQueueSend(doneQueue, &queueData, QUEUE_DELAY);
   for(;;) {
-    if(xQueueReceive(vidQueue, &queueData, QUEUE_DELAY) != pdPASS){
-      continue;
-    }
+    // black to wait for loopTask to notify this task
+    ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+    // copy the received data
+    memcpy(rxBufferCopy, rxBuffer, RECEIVE_LINE_BYTES);
+    //notify the serial task that it can receive more data
+    xTaskNotifyGive(serialTask);
+    //clear the display buffer to remove junk from previous transmission
+    memset(displayBuffer, 0, RECEIVE_LINE_BYTES);
     memcpy(displayBuffer, rxBufferCopy, RECEIVE_LINE_BYTES);
-    //clear the receive buffer
-    memset(rxBufferCopy, 0, RECEIVE_LINE_BYTES);
-    //displayBuffer[RECEIVE_LINE_BYTES] = 0;
     M5.Lcd.setCursor(0, row);
     M5.Lcd.print(displayBuffer); 
-    blink();
-    //we need to notify the UART task that we can receive data
-    xQueueSend(doneQueue, &queueData, QUEUE_DELAY);  
+    row += 8;
+    if(row >= 80){
+      row = 0;
+      M5.Lcd.fillScreen(BLACK);
+    }   
+  }
+}
+
+void serial_task(void * parameter){
+  for(;;){
+//    size_t xBytesSent;
+    //get notified by the display task, or time out on the initial processing
+    ulTaskNotifyTake(pdTRUE, ( TickType_t ) 100 );
+    int read = Serial.readBytes(rxBuffer, RECEIVE_LINE_BYTES);
+    if(read == 0)
+    {
+      Serial.write(VSYNC);
+      return;
+    }
+    //send notification to displayTask, bringing it out of Blocked state
+    xTaskNotifyGive(displayTask);
   }
 }
 
 void setup() {
-M5.begin();
+  M5.begin();
   //horizontal rotation
   M5.Lcd.setRotation(3);
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(1);
   pinMode (ledPin, OUTPUT);
 
-  //render queue
-  vidQueue = xQueueCreate(1, sizeof(uint32_t *));
-  //done queue
-  doneQueue = xQueueCreate(1, sizeof(uint32_t *));
-  
-  //setup my core 0 task
+
+    //setup my core 1 task
+  xTaskCreatePinnedToCore(
+      serial_task, //task callback
+      "SerialTask",// name of the task 
+      10000, //task stack size
+      NULL,  //task input parameter
+      0,     //Priority of the task - 0 = lowest */
+      &serialTask, //task handle
+      1);  //task core (0 is available, 1 is used by arduino loop()
+
+    //setup my core 0 task
   xTaskCreatePinnedToCore(
       display_task, //task callback
       "DisplayTask",// name of the task 
       10000, //task stack size
       NULL,  //task input parameter
-      0,     //Priority of the task - 0 = lowest */
+      1,     //Priority of the task - 0 = lowest */
       &displayTask, //task handle
       0);  //task core (0 is available, 1 is used by arduino loop()
-  
+
   Serial.begin(BAUD_RATE);
-  Serial.setTimeout(5000);
+  Serial.setTimeout(10000);
 }
-
-void blink(){
-  digitalWrite (ledPin, LOW);  // turn on the LED
-  delay(5); // wait for half a second or 500 milliseconds
-  digitalWrite (ledPin, HIGH); // turn off the LED
-}
-
-int temp = 0;
 
 void loop() {
-  int *queueData = &temp;
-  int *doneQueueData;
-
-  //get notified by the display task
-  xQueueReceive(doneQueue, queueData, QUEUE_DELAY);
-  int read = Serial.readBytes(rxBuffer, RECEIVE_LINE_BYTES);
-  if(read == 0)
-  {
-    Serial.write(VSYNC);
-    return;
-  }
-  memcpy(rxBufferCopy, rxBuffer, RECEIVE_LINE_BYTES);
-  xQueueSend(vidQueue, queueData, QUEUE_DELAY); 
-  
-  row += 8;
-  if(row >= 80){
-    row = 0;
-    M5.Lcd.fillScreen(BLACK);
-  }
+  digitalWrite (ledPin, LOW);  
+  delay(500); 
+  digitalWrite (ledPin, HIGH);
+  delay(500);  
 } 
